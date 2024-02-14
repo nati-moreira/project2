0dd65a93cab6d8a009a4a6988179098056213902
nati-moreira <158375023+nati-moreira@users.noreply.github.com>
2024-02-13 21:29:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,18 @@ if hist_button: #se o botão for clicado
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+
+
+car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
+
+# Criar botão para o gráfico de dispersão
+scatter_button = st.button('Criar gráfico de dispersão')
+
+if scatter_button: # Se o botão for clicado
+    st.write('Criando um gráfico de dispersão para odometer vs price') # Escrever uma mensagem
     
+    # Criar o gráfico de dispersão
+    fig = px.scatter(car_data, x="odometer", y="price")
+    
+    # Exibir o gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
